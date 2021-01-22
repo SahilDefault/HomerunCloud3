de6116b3c447134f7b72e3418e15168476d752dd
SahilDefault <sahil@getdefault.in>
2021-01-22 18:48:28 +0530

    Add argument to chrome options

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--window-size=1920,1080")
 op.add_argument("--headless")
-op.add_argument("--disable-dev-sh-usage")
+op.add_argument('--disable-dev-shm-usage')
 op.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
