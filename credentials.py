5d34890ef8a2bde7c7ee21345dd9efec3e1e1a39
SahilDefault <sahil@getdefault.in>
2021-01-23 02:08:32 +0530

    Change Password

--- credentials.py
+++ credentials.py
@@ -2,6 +2,6 @@ user_email = "sk0196146@gmail.com"
 user_pass = "P@ssw0rd9"
 
 emailAddress = "sahilkanger27@gmail.com"
-emailPassword = "Sahil@1234"
+emailPassword = "Sahil@321"
 
 send_mail_to = "sahil@getdefault.in"
